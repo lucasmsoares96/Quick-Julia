f3b3275f8f1f0e04408c90b4b3d49f9d8a8d229a
Lucas Martins Soares <lucasmsoares96@gmail.com>
2023-04-25 18:08:07 -0300

    Update and rename pluto to pluto.sh

--- pluto.sh
+++ pluto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 podman run --rm -v ./:/pluto/notebooks/ -p 28761:28761 docker.io/lucasmsoares96/quick-julia:latest
 
-# Salve esta página como favorit
+# Salve esta página como favorito
 # http://localhost:28761
 
